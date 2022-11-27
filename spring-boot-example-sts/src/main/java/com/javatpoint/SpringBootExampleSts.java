88d3feab0fe932075168742216ddfe736129be70
User <abeedshaik997@gamil.com>
2022-11-27 11:01:30 +0530

    first commit

---
+++ spring-boot-example-sts/src/main/java/com/javatpoint/SpringBootExampleSts.java
@@ -0,0 +1,15 @@
+package com.javatpoint;
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+@SpringBootApplication 			////similar to @Configuration @EnableAutoConfiguration @ComponentScan 
+public class SpringBootExampleSts 
+{
+public static void main(String[] args)
+{  
+SpringApplication.run(SpringBootExampleSts.class, args);  
+System.out.println("welcome");
+System.out.println("welcome to my first spring boot application");
+System.out.println("welocme");
+
+
+}}
